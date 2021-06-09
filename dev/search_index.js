aa3b03d1b2fa5d01eb709ef767660dde2b5cbcea
Documenter.jl <documenter@juliadocs.github.io>
2021-06-09 12:53:29 +0000

    build based on 2152f21

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CryptoTools","category":"page"},{"location":"#CryptoTools","page":"Home","title":"CryptoTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CryptoTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CryptoTools]","category":"page"}]
+}
